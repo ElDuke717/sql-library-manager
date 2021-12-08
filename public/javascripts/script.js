60829a4f2c9a439298f0a3dfee6bd616210aa97c
Nick Huemmer <njhuemmer@gmail.com>
2021-12-08 14:24:10 -0500

    Search feature implemented, working on 'no results found' display

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,30 @@
 'use strict'
 
-/**
- * This is where any JS will go for search bar or pagination.
- */
+// ------------------------------------------
+//  GLOBAL VARIABLES
+// ------------------------------------------
+
+
+const searchButton = document.getElementById('search-submit');
+
+
+
+searchButton.addEventListener('click', () => {
+    searchBooks();
+});
+
+function searchBooks() {
+    const searchInput = document.querySelector('.search-input').value.toLowerCase();
+    console.log(searchInput);
+    const bookItem = document.querySelectorAll('.book-item');
+    const bookTable = document.querySelector('.book-table');
+    const bookList = document.querySelector('.book-list');
+    for (let i = 0; i < bookItem.length; i++) {
+        if(bookItem[i].innerText.toLowerCase().includes(searchInput)) {
+            bookItem[i].style.display='';
+        } else {
+            bookItem[i].style.display='none';
+        }
+    } 
+    
+}
