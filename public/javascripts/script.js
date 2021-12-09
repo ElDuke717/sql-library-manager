fb60c9bdaeef60dba1ee66d4b9bb5151ca32b78d
Nick Huemmer <njhuemmer@gmail.com>
2021-12-09 10:32:22 -0500

    begin work on pagination

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,10 +4,11 @@
 //  GLOBAL VARIABLES
 // ------------------------------------------
 
-
 const searchButton = document.getElementById('search-submit');
-
-
+const bookTable = document.querySelector('.book-table');
+const bookItem = document.querySelectorAll('.book-item');
+const linkList = document.querySelector('.link-list');
+const itemsPerPage = 15;
 
 searchButton.addEventListener('click', () => {
     searchBooks();
@@ -16,15 +17,43 @@ searchButton.addEventListener('click', () => {
 function searchBooks() {
     const searchInput = document.querySelector('.search-input').value.toLowerCase();
     console.log(searchInput);
-    const bookItem = document.querySelectorAll('.book-item');
-    const bookTable = document.querySelector('.book-table');
-    const bookList = document.querySelector('.book-list');
+    //const bookItem = document.querySelectorAll('.book-item');
+    
     for (let i = 0; i < bookItem.length; i++) {
         if(bookItem[i].innerText.toLowerCase().includes(searchInput)) {
             bookItem[i].style.display='';
         } else {
             bookItem[i].style.display='none';
         }
-    } 
-    
+    }
+    // console.log(bookArray.length); 
+    // if(bookArray.length === 0) {
+    //     bookTable.innerHTML = `<h2>cannot be found</h2>`;
+    //     bookArray.push(searchInput);
+    // }
+}
+
+function showPage(list, page) {
+    const startIndex = (page * itemsPerPage) - itemsPerPage;
+    const endIndex = page * itemsPerPage;
+
+}
+
+function addPagination() {
+    console.log(bookItem.length)
+    const numOfPages = Math.ceil(bookItem.length/itemsPerPage);
+    linkList.innerHTML="";
+    console.log(numOfPages)
+    for (let i = 1; i<= numOfPages; i++) {
+        const button = `<li> <button type = "button">${[i]}</button> </li>`;
+        linkList.insertAdjacentHTML("beforeend", button);
+        document.querySelector(".link-list button").className="active";
+    }
+    linkList.addEventListener("click", (e) => {
+        if(e.target.tagName ==="BUTTON") {
+            document.querySelector('.active').className="";
+            e.target.className="active";
+            showPage(list, e.target.textContent);
+        }
+    });
 }
